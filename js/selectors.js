f139641857daf18becdfd79843072409bda55d17
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-21 19:26:47 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -11,7 +11,7 @@ function lastImageSelector(){
  //The function should use the last jQuery selector to return the last image in index.html 
   
 
-   $( img="cat"); 
+  return $(img="cat"); 
    
   
 }
