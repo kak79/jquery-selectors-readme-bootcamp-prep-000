be32e6b320000109657beeb1a8597c88ad65c7b8
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-21 19:54:56 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,14 +1,11 @@
 // declare your functions here...
-
 function paragraphSelector(){
-  //The function should use an element selector to select the p tags in index.html and return those elements.
   
   return $("p");
   
 }
 
 function lastImageSelector(){
- //The function should use the last jQuery selector to return the last image in index.html 
   
   return $("#cat"); 
    
@@ -16,23 +13,18 @@ function lastImageSelector(){
 }
  
 function ninjaBabySelector(){
- //The function should use an ID selector to return the ninja baby image. 
   
   return $("#baby-ninja");
   
 }
 
 function divSelector(){
- //The function should use a class selector to return the two divs with the class pics 
   
   return $(".pics");
   
 }
 
 function firstListItem(){
- //The function should use a first-child selector to return the first list item in the ul with the ID pic-list 
   
-   return $(li:first-child("pic-list"));
   
 }
-
